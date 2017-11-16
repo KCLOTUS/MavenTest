e64654c2debd412884b7ebbf2880ec5c50057ba2
15179156337 <2350602201@qq.com>
2017-11-16 22:00:59 +0800

    yes

---
+++ src/main/java/cn/naturemix/MavenTest/util/StringUtil.java
@@ -0,0 +1,27 @@
+package cn.naturemix.MavenTest.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类
+ */
+public final class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String str){
+        if (str != null){
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否排空
+     */
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
+
+}
